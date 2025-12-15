80dafa43c4510fa09365e67e9e6b4ebcdd343b81
moses1252 <amviramontes34@yahoo.com>
2025-12-14 22:11:04 -0800

    Initial Spring Boot project setup

---
+++ src/test/java/com/nba/stats_api/StatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.stats_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
