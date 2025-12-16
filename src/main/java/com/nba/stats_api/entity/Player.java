eff861a6927efefdbef754df12977e12095ae8a4
moses1252 <amviramontes34@yahoo.com>
2025-12-15 21:18:07 -0800

    created my player entity and the table was created in pgadmin

--- src/main/java/com/nba/stats_api/entity/Player.java
+++ src/main/java/com/nba/stats_api/entity/Player.java
@@ -1,5 +1,29 @@
 package com.nba.stats_api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name="players")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Player {
+
+    @Id
+    @Column(name="player_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="name")
+    private String name;
+    @Column(name="position")
+    private String position;
+    @Column(name="age")
+    private Integer age;
+    @Column(name="team")
+    private String team;
 
 }
